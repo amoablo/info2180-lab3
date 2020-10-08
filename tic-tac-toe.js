93d29b5d78337a7fca20c3e9124bccf40a0af7a3
Your Name <you@example.com>
2020-10-08 08:40:39 -0500

    COMPLETED EXERCISE 1

---
+++ tic-tac-toe.js
@@ -0,0 +1,12 @@
+
+var options = ["square.X","square.O"];
+
+    function ready(){
+        document.getElementById("board").children.className="square";
+    };
+
+    document.addEventListener("DOMContentLoaded",ready);
+
+
+
+
